9b7f1343afced494885cd504e2f365ed9c7b009a
pvvkrishnamraju <pvvkrishnamraju@gmail.com>
2017-01-21 14:28:16 +0530

    adding semicolon at the end in schema.sql

--- iae-app/sql/schema.sql
+++ iae-app/sql/schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE roles (id INT(50) NOT NULL AUTO_INCREMENT, name VARCHAR(80) DEFAULT NULL, description VARCHAR(80) DEFAULT NULL, 
   PRIMARY KEY  (id)
-) ENGINE=INNODB DEFAULT CHARSET=utf8
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 CREATE TABLE users (id int(20) NOT NULL, name varchar(20) NOT NULL, mobile varchar(15) NOT NULL, 
   password varchar(80) NOT NULL, email varchar(50) default NULL, role int(20) NOT NULL,
   PRIMARY KEY  (id),
   UNIQUE KEY uc_username (name)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 -- Constraints
