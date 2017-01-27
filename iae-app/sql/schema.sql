2f823b15fc8154c06743343ac83a6f76de3605e4
vikramadda <aditya_adda@yahoo.co.in>
2017-01-27 18:02:29 +0530

    Updated SQL Schema

--- iae-app/sql/schema.sql
+++ iae-app/sql/schema.sql
@@ -1,16 +1,29 @@
-CREATE TABLE roles (id INT(50) NOT NULL AUTO_INCREMENT, name VARCHAR(80) DEFAULT NULL, description VARCHAR(80) DEFAULT NULL, 
-  PRIMARY KEY  (id)
-) ENGINE=INNODB DEFAULT CHARSET=utf8
+--Roles table
+CREATE TABLE roles (
+  id bigint(50) NOT NULL AUTO_INCREMENT,
+  name varchar(80) default NULL,
+  description varchar(80) default NULL,
+  PRIMARY KEY  (id),
+  UNIQUE KEY uc_rolename (name),
+  KEY idx_rolename (name)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE users (id int(20) NOT NULL, name varchar(20) NOT NULL, mobile varchar(15) NOT NULL, 
-  password varchar(80) NOT NULL, email varchar(50) default NULL, role int(20) NOT NULL,
+--Users table
+CREATE TABLE users (
+  id int(20) NOT NULL auto_increment,
+  name varchar(20) NOT NULL,
+  mobile varchar(15) NOT NULL,
+  password varchar(80) NOT NULL,
+  email varchar(50) default NULL,
+  role int(20) NOT NULL,
+  securityquestions varchar(200) default NULL,
   PRIMARY KEY  (id),
   UNIQUE KEY uc_username (name)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
--- Constraints
-ALTER TABLE roles ADD CONSTRAINT uc_rolename UNIQUE (name);
-
--- Indexes
-CREATE INDEX idx_rolename ON roles ( NAME );
+--Security Questions table
+CREATE TABLE securityquestions (
+  id int(80) NOT NULL,
+  name varchar(80) default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
