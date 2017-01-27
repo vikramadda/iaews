0f11c59d692e06948de62098b111a091f28a989e
vikramadda <aditya_adda@yahoo.co.in>
2017-01-27 18:49:56 +0530

    Static Data updated with security questions

--- iae-app/sql/staticdata.sql
+++ iae-app/sql/staticdata.sql
@@ -1,5 +1,11 @@
-insert into `roles` (`id`, `name`, `description`) values('1','Admin','Administrator');
-insert into `roles` (`id`, `name`, `description`) values('2','User','User');
-insert into `roles` (`id`, `name`, `description`) values('3','Guest','Guest');
-insert into `roles` (`id`, `name`, `description`) values('4','Volunteer','Volunteer');
-insert into `roles` (`id`, `name`, `description`) values('5','Internship','Internship');
+insert into roles (id, name, description) values(1,'Admin','Administrator');
+insert into roles (id, name, description) values(2,'User','User');
+insert into roles (id, name, description) values(3,'Guest','Guest');
+insert into roles (id, name, description) values(4,'Volunteer','Volunteer');
+insert into roles (id, name, description) values(5,'Internship','Internship');
+
+insert  into securityquestions(id,name) values (1,'What is Your nick name ?');
+insert  into securityquestions(id,name) values (2,'What is Your home town ?');
+insert  into securityquestions(id,name) values (3,'What is Your favourite city ?');
+insert  into securityquestions(id,name) values (4,'What is Your favourite drink ?');
+insert  into securityquestions(id,name) values (5,'What is Your favourite food ?');
