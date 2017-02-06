02439d718ec2196ecb50aa45a92d1692882b14a7
pvvkrishnamraju <pvvkrishnamraju@gmail.com>
2017-02-06 10:42:21 +0530

    styling

--- iae-ui/app/app.component.ts
+++ iae-ui/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<app-header></app-header><div style="min-height:500px;"><router-outlet></router-outlet></div><app-footer></app-footer>
+  template: `<app-header></app-header><div><router-outlet></router-outlet></div><app-footer></app-footer>
   `
 })
 export class AppComponent  {}
