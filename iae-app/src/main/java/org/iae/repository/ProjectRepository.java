93c39eb10c7343f6987c3c48a4d59f0b1602ec17
pvvkrishnamraju <pvvkrishnamraju@gmail.com>
2017-02-27 00:09:36 +0530

    projects and activities changes

--- iae-app/src/main/java/org/iae/repository/ProjectRepository.java
+++ iae-app/src/main/java/org/iae/repository/ProjectRepository.java
@@ -3,14 +3,13 @@ package org.iae.repository;
 import java.util.List;
 
 import org.iae.pojo.Project;
-import org.springframework.data.domain.Page;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository  extends CrudRepository<Project, Long> {
 
-	Page<Project> findAll();
+	List<Project> findAll();
 	
 	Project findByName(String name);
 	
